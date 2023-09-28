914529bd8e6f0e2f40976a6b997c8f68905d8cc7
Many Kasiriha <many.kasiriha@dbschenker.com>
2023-09-28 18:40:40 +0200

    Add popup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import "./popup";
 
 console.log('Script started successfully');
 
---
+++ src/popup.ts
@@ -0,0 +1,39 @@
+/// <reference types="@workadventure/iframe-api-typings" />
+
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+let currentPopup: any = undefined;
+var isCoWebSiteOpened =  false;
+var urlWA ='https://db-planet.deutschebahn.com/pages/telefonie/apps/content/workadventure';
+
+function closePopup(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+WA.room.area.onEnter("boothWa").subscribe(() => {
+    currentPopup =  WA.ui.openPopup("popupWa","Interested in discovering the opportunities presented by our virtual environments, offices and events? Reach out to us or visit our DB Planet page for more details",
+    [
+        {
+            label: "DB Planet",
+            callback: (popup) => {
+                WA.nav.openTab(urlWA)
+                closePopup();
+            }
+        },
+        {
+            label: "Close",
+            callback: (popup) => {
+                closePopup();
+            }
+        }
+        ]);
+});
+
+WA.room.area.onLeave("boothWa").subscribe(() => {
+    closePopup();
+})
+
+export {};
